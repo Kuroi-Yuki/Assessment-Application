eb6fd76945fcebd5b07c0f3add35dfc168ad66a2
shamtastic <b00046058@aus.edu>
2016-05-19 23:01:12 +0400

    Updated Controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -105,7 +105,6 @@ angular.module('starter.controllers', [])
   illuminance = values[0];
 /*  $scope.humidity= values[1];
   $scope.temp=values[2]; */
-  //THI = (temp) - (0.55 - 0.55 *(humidity/1000)) * (temp - 58);
   THI = C1 + (C2 * T) + (C3 * R) + (C4 * T * R) + (C5 * T2) + (C6 * R2) + (C7 * T2 * R) + (C8 * T * R2) + (C9 * T2 * R2);
 
 }
